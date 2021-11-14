c03cc8ec9cb675dd1f7eaa84ff0d06d886058555
Nethmika Broo <92091962+darshanamk@users.noreply.github.com>
2021-11-14 21:40:45 +0530

    Update youtube.js

--- plugins/youtube.js
+++ plugins/youtube.js
@@ -56,7 +56,7 @@ const KSK = Config.WORKTYPE == 'public' ? false : true
         ytgot += '🧞‍♂️ *' + video.title + '*' + '\n' + '*🧚‍♀️Duration:-* ' +  video.duration +  '\n' + '*📎Link:-* ' + video.url + '\n'+ '*📅time ago:-* ' + video.ago + '\n\n'
     });
 
-    await message.client.sendMessage(message.jid, '*❖  Lusifar youtube search Engine ❖*\n' + 'Result ...' + '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ytgot,MessageType.text, {quoted: message.data});
+    await message.client.sendMessage(message.jid, '*❖ ⚕️NETHMIKA BOT⚕️ youtube search ❖*\n' + 'Result ...' + '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ytgot,MessageType.text, {quoted: message.data});
 }));
 
 lusifar.addCommand({pattern: 'yt ?(.*)', fromMe: true,  deleteCommand: false, desc: Lang.YT_DESC}, (async (message, match) => { 
@@ -75,5 +75,5 @@ lusifar.addCommand({pattern: 'yt ?(.*)', fromMe: true,  deleteCommand: false, de
         ytgot += '🧞‍♂️ *' + video.title + '*' + '\n' + '*🧚‍♀️Duration:-* ' +  video.duration +  '\n' + '*📎Link:-* ' + video.url + '\n'+ '*📅time ago:-* ' + video.ago + '\n\n'
     });
 
-    await message.client.sendMessage(message.jid, '*❖  Lusifar youtube search Engine ❖*\n' + 'Result ...' + '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ytgot,MessageType.text, {quoted: message.data});
+    await message.client.sendMessage(message.jid, '*❖ ⚕️NETHMIKA BOT⚕️ youtube search ❖*\n' + 'Result ...' + '\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n\n' + ytgot,MessageType.text, {quoted: message.data});
 }));
