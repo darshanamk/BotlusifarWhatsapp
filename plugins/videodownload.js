8d6711c03194e71602547ddb536fded973a617a9
Nethmika Broo <92091962+darshanamk@users.noreply.github.com>
2021-11-14 21:49:37 +0530

    Update videodownload.js

--- plugins/videodownload.js
+++ plugins/videodownload.js
@@ -5,9 +5,9 @@ const axios = require('axios');
 const { errorMessage, infoMessage } = require('../helpers');
 const YTV_DESC = "Youtube Video Downloader "
 const YT_NEED = "*need word!.*"
-const DWLOAD_VID = "*🎭Downloading Your Video...*"
-const YTV_UP = "*🚀Uploading Your Video...*"
-const NO_RESULT = "*🌀can't Find Anything...*"
+const DWLOAD_VID = "*⚕️NETHMIKA BOT⚕️ 😎(っ◔◡◔)っDownloading Your Video...*"
+const YTV_UP = "*⚕️NETHMIK BOT⚕️ 🤪𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴 𝗬𝗼𝘂𝗿 𝗩𝗶𝗱𝗲𝗼...*"
+const NO_RESULT = "*⚕️NETHMIKA BOY⚕️ 🎃𝕔𝕒𝕟'𝕥 𝔽𝕚𝕟𝕕 𝔸𝕟𝕪𝕥𝕙𝕚𝕟𝕘...*"
 
 
 //උස්සන්නද ආවෙ බේසිකයෝ.බොහොම අමාරුවෙන් හැදුවෙ.උස්සන එකා අවජාතකයෙක් කියල හිතාගන්න පුලුවන් පොන්න හැත්ත.හුකන පොන්නයෝ
